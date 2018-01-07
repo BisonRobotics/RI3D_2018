d73c8ddb91e59c9e264de5f1f4fc9be4dc30ae72
YoloSwagDogDiggity <ultimate360solutions@gmail.com>
2018-01-06 23:50:07 -0600

    Set up basic commands

--- Greenhorns2018/src/org/usfirst/frc4607/Greenhorns2018/OI.java
+++ Greenhorns2018/src/org/usfirst/frc4607/Greenhorns2018/OI.java
@@ -8,7 +8,6 @@
 // update. Deleting the comments indicating the section will prevent
 // it from being updated in the future.
 
-
 package org.usfirst.frc4607.Greenhorns2018;
 
 import org.usfirst.frc4607.Greenhorns2018.commands.*;
--- Greenhorns2018/src/org/usfirst/frc4607/Greenhorns2018/commands/Driving.java
+++ Greenhorns2018/src/org/usfirst/frc4607/Greenhorns2018/commands/Driving.java
@@ -1,19 +1,18 @@
-package org.usfirst.frc4607.Test.commands;
-
-import org.usfirst.frc4607.Test.Robot;
-import org.usfirst.frc4607.Test.RobotMap;
+package org.usfirst.frc4607.Greenhorns2018.commands;
+import org.usfirst.frc4607.Greenhorns2018.Robot;
+import org.usfirst.frc4607.Greenhorns2018.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class FeedingCommand extends Command {
-	double speed;
-    public FeedingCommand(double speed) {
+public class Driving extends Command {
+
+    public Driving() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    	this.speed = speed;
+    	requires(Robot.drivetrain);
     }
 
     // Called just before this Command runs the first time
@@ -22,29 +21,22 @@ public class FeedingCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    /*	if(timeSinceInitialized()%.75 <= .25) Robot.shooter.startFeeding = true;
-    	else Robot.shooter.startFeeding = false;
-    	if(RobotMap.shooterShooterFeed.get() == 0){*/
-    		RobotMap.shooterShooterFeed.set(speed);
-    	/*}
-    	else{
-    		RobotMap.shooterShooterFeed.set(0);
-    	}*/
+    	RobotMap.drivetrainDifferentialDrive.tankDrive(Robot.oi.driver.getX(), Robot.oi.driver.getY());
     }
-    
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return true;
+        return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	
+    	RobotMap.drivetrainDifferentialDrive.tankDrive(0, 0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
